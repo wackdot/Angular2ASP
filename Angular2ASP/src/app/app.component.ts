b82c9022ae5797f7d2c851d5d24d799188498b17
Gab <wackdot2002@gmail.com>
2018-09-15 22:23:02 +0800

    Importing project

---
+++ Angular2ASP/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+    template: `
+        <h1>Hello {{name}}</h1>
+        <h2>Hello!</h2>
+
+`,
+})
+export class AppComponent  { name = 'Angular'; }
